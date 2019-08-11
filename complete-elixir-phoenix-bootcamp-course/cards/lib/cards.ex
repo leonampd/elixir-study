de542aed93b11a86bef79f96cb6793cd81c365f5
Leonam Pereira Dias <leonam.pd@gmail.com>
2019-08-11 06:11:01 -0300

    section 2: pattern matching

--- complete-elixir-phoenix-bootcamp-course/cards/lib/cards.ex
+++ complete-elixir-phoenix-bootcamp-course/cards/lib/cards.ex
@@ -4,10 +4,35 @@ defmodule Cards do
   """
 
   def create_deck do
-    ["Ace", "Two", "Three"]
+    values = ["Ace", "Two", "Three", "Four", "Five"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+
+    for suit <- suits, value <- values do
+      "#{value} of #{suit}"
+    end
   end
 
   def shuffle(deck) do
     Enum.shuffle(deck)
+  end
+
+  def deal(deck, hand_size) do
+    Enum.split(deck, hand_size)
+  end
+
+  def save(deck, filename) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(filename, binary)
+  end
+
+  def load(filename) do
+    case File.read(filename) do
+      {:ok, binary} -> :erlang.binary_to_term(binary)
+      {:error, _reason} -> "The file does not exist"
+    end
+  end
+
+  def create_hand(hand_size) do
+    Cards.create_deck |> Cards.shuffle |> Cards.deal(hand_size)
   end
 end
